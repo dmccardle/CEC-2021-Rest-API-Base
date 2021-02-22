757f8a47a7e5adf692822f98ffa676fc08c2a283
dmccardle <danny.mccardle@unb.ca>
2021-02-22 16:52:17 -0400

    Added routes to pick best car (lowest price) and get all cars

--- app.py
+++ app.py
@@ -17,15 +17,19 @@ class CarSchema(ma.Schema):
   class Meta:
     fields = ('name', 'cost', 'mileage')
 
+car_schema = CarSchema()
+cars_schema = CarSchema(many=True)
 
 @app.route('/')
 def get_best_car():
   cars = parse_car_data_from_file()
-  best_car = get_best_car_from_list(cars)
-  print(cars)
-  return ""
-  pass
+  best_car = pick_best_car_from_list(cars)
+  return car_schema.jsonify(best_car)
 
+@app.route('/all')
+def get_all_cars():
+   cars = parse_car_data_from_file()
+   return cars_schema.jsonify(cars)
 
 def parse_car_data_from_file():
   list_of_cars = []
@@ -42,13 +46,20 @@ def parse_car_data_from_file():
 def get_car_data_from_line(line):
   car_data = line.split(',')
   car_name = car_data[0]
-  car_mileage = car_data[1]
-  car_cost = car_data[2]
+  car_mileage = int(car_data[1])
+  car_cost = int(car_data[2])
   car = Car(car_name, car_mileage, car_cost)
   return car
 
-def get_best_car_from_list(list_of_cars):
-  pass
+def pick_best_car_from_list(list_of_cars):
+  if not list_of_cars:
+    return
+  
+  best_car = list_of_cars[0]
+  for i in range(1, len(list_of_cars)):
+    if list_of_cars[i].cost < best_car.cost:
+      best_car = list_of_cars[i]
+  return best_car
 
 
 if __name__ == '__main__':
