5416e3043a3a9b3768cc92fba2d478b62204ceb7
dmccardle <danny.mccardle@unb.ca>
2021-02-22 16:27:07 -0400

    Added base structure of API, added data files, created gitignore for env/ files

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-print('Hello, world!')
+from flask import Flask
+from flask_cors import CORS
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+CORS(app)
+ma = Marshmallow(app)
+
+class Car():
+  def __init__(self, name, mileage, cost):
+    self.name = name
+    self.mileage = mileage
+    self.cost = cost
+
+class CarSchema(ma.Schema):
+  class Meta:
+    fields = ('name, cost, mileage')
+
+
+@app.route('/')
+def get_best_car():
+  parse_car_data_from_file()
+  pass
+
+
+def parse_car_data_from_file():
+  pass
